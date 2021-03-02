3adb4abba6232eaf7d71ac5cfee0f37c1b104e27
Mark Jabel Frias <friasmarkjabel@gmail.com>
2021-03-02 09:25:21 +0800

    Add display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcFrameComponent } from './calc-frame/calc-frame.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalcFrameComponent
+    CalcFrameComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
